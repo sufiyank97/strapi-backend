0a7535985d82b26d7e0f55ffa492131314ed06f3
sufiyan <sufiyan7697@gmail.com>
2020-08-27 01:27:58 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fa47c2547f69efc914bbb1e03088ebcc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7e46ddfe-845b-4fb0-b880-bb429e240861'
+};
