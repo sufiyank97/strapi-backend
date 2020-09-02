be891ad05f483d6c3a41f5fa26992d4909a733de
sufiyan <sufiyan7697@gmail.com>
2020-09-02 20:14:35 +0530

    new record added in podcasts api

--- config/database.js
+++ config/database.js
@@ -7,10 +7,12 @@ module.exports = ({ env }) => ({
         // host: env('DATABASE_HOST', '127.0.0.1'),
         // srv: env.bool('DATABASE_SRV', false),
         // port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'kiaOrana'),
+        // database: env('DATABASE_NAME', 'kiaOrana'),
         // username: env('DATABASE_USERNAME', ''),
         // password: env('DATABASE_PASSWORD', ''),
-        uri: "mongodb://sufiyan2:Sufiyan2@cluster0-shard-00-00.ooi6z.mongodb.net:27017,cluster0-shard-00-01.ooi6z.mongodb.net:27017,cluster0-shard-00-02.ooi6z.mongodb.net:27017/kiaOrana?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+        // uri: "mongodb://sufiyan2:Sufiyan2@cluster0-shard-00-00.ooi6z.mongodb.net:27017,cluster0-shard-00-01.ooi6z.mongodb.net:27017,cluster0-shard-00-02.ooi6z.mongodb.net:27017/kiaOrana?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+        uri: `${env.DATABASE_URI}`,
+        database: `${env.DATABASE_NAME}`
       },
       options: {
         // ssl: true
